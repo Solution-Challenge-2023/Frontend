8b71fbc217049ef5d7c697b18b1010daeaef101f
Chandrala <gudrms2538@gmail.com>
2023-03-30 21:21:21 +0900

    no message

--- lib/colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-@immutable
-class Colors {
-  final mainblue = const Color(0xFF0597F2);
-  final mainorange = const Color(0xFFFBA15D);
-
-  const Colors();
-}
