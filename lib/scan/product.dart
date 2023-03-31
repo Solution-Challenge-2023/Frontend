10e72ca344578e9f401051d649da8fd95b29cd3e
yunju727 <80828786+yunju727@users.noreply.github.com>
2023-04-01 01:05:26 +0900

    Update product.dart

--- lib/scan/product.dart
+++ lib/scan/product.dart
@@ -1,26 +1,41 @@
-// import 'package:flutter/material.dart';
-//
-// class Product{
-//   //properties
-//   int id;
-//   String itemName;
-//   int count;
-//   int categoryId;
-//   int refrigeratorId;
-//   int dday;
-//   String expiredAt;
-//   String createdAt;
-//   String updateAt;
-//
-//   Product({
-//     required this.id,
-//     required this.itemName,
-//     required this.count,
-//     required this.categoryId,
-//     required this.refrigeratorId,
-//     required this.dday,
-//     required this.expiredAt,
-//     required this.createdAt,
-//     required this.updateAt
-//   });
-// }
+import 'package:flutter/material.dart';
+enum ExpiredType { sellByDate, expiryDate }
+
+class Product {
+  String itemName;
+  int count;
+  int categoryId;
+  int refrigeratorId;
+  String expiredAt;
+  ExpiredType expiredType;
+
+  Map<String, dynamic> toJson() {
+    return {
+      'itemName': itemName,
+      'count': count,
+      'expiredAt': expiredAt,
+    };
+  }
+
+  Product({
+    required this.itemName,
+    required this.count,
+    required this.categoryId,
+    required this.refrigeratorId,
+    required this.expiredAt,
+    required this.expiredType,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      itemName: json['itemName'],
+      count: json['count'],
+      categoryId: json['categoryId'],
+      refrigeratorId: json['refrigeratorId'],
+      expiredAt: json['expiredAt'],
+      expiredType: json['expiredType'] == 'SELL_BY_DATE'
+          ? ExpiredType.sellByDate
+          : ExpiredType.expiryDate,
+    );
+  }
+}
